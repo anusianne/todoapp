cf45f8c8ba39d3b1afd9c907d423e1f48c27f5f6
Anna Zielinska <ann.ka.zielinska@gmail.com>
2022-04-12 13:44:56 +0200

    home page and basic add task form

---
+++ projects/task/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/adapters/primary/ui/add-task.component-module';
---
+++ src/app/pages/add-task.page-module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AddTaskPage } from './add-task.page';
+import { AddTaskComponentModule } from '../../../projects/task/src/lib/adapters/primary/ui/add-task.component-module';
+
+@NgModule({ imports: [CommonModule, 
+      RouterModule.forChild([
+        {
+          path: '',
+          component: AddTaskPage,
+        }
+      ]),
+  AddTaskComponentModule
+],
+  	declarations: [AddTaskPage],
+  	providers: [],
+  	exports: [] })
+export class AddTaskPageModule {
+}
